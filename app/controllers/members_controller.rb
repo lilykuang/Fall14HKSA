3514088c0a103ce07cbe0d7453e6a455c4fabf94
Your Name <your.email@example.com>
2014-08-23 14:03:39 -0700

    Removed Editors

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,26 @@
 class MembersController < ApplicationController
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      flash[:success] = "Welcome to HKSA!"
+      redirect_to root 
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:firstName, :lastName :email)
+    end
 end
