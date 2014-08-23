0636643a27889253c7af90697bf421ec45f88780
Your Name <your.email@example.com>
2014-08-23 12:42:25 -0700

    Add Front page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  #front_page 
+  root 'front_pages#index'
+
   resources :users
   resources :events
   resources :sessions, only: [:new, :create, :destroy]
+
+  #officer login 
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+
+
 end
---
+++ test/controllers/front_pages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FrontPagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
